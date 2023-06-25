238a5acca97d87c7b42ee1c3fd3d006d1e31c713
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-06-25 13:22:54 +0300

    Updated MongoDB Connection

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Joseph:12345@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Joseph:12345@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Joseph:12345@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Joseph:12345@testcluster.1djsdy4.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Joseph:12345@testcluster.1djsdy4.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Joseph:12345@testcluster.1djsdy4.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+
+
+mongodb+srv://Joseph:<password>@testcluster.1djsdy4.mongodb.net/?retryWrites=true&w=majority
